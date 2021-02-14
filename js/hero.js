d6a1512a88696bfdb52c8f923f5ef4be80522eb4
Om Tejaswi <omtejaswi2008classes@gmail.com>
2021-02-14 08:06:53 +0530

    updated

--- js/hero.js
+++ js/hero.js
@@ -1,6 +1,11 @@
 class createHero {
     constructor(x,y,r) {
 
+        var physycs = {
+            density: 1,
+            frictionAir: 1
+        }
+
         this.body = Bodies.circle(x,y,r);
         World.add(world,this.body);
 
